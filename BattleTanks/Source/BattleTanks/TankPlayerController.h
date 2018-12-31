9506f1ef0b2564f2db82ffe2cb80ada72d9b44fc
thecoldlenny <thecoldlenny@gmail.com>
2018-12-31 18:24:13 -0500

    BT15 Added Tank and TankPlayerController C++ classes  Reparent the blueprints to these new classes

---
+++ BattleTanks/Source/BattleTanks/TankPlayerController.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "Tank.h"
+#include "TankPlayerController.generated.h" // Must be the last hash include
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANKS_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	public: 
+		ATank* GetControlledTank() const;
+
+};
